bd4f564b809542090d4f130629bde093dd75fb60
Dark Soul <maheshh.kumar1508@gmail.com>
2025-12-17 14:43:07 +0530

    type narrowing and guarding

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,32 @@
-function greet(name:string):string{
-    return `Good morning, ${name}`;
+function greet(name: string): string {
+  return `Good morning, ${name}`;
 }
-console.log(greet('mahesh'));
+console.log(greet("mahesh"));
+
+let apiResponseStatus: "done" | "pending" | "error" = "done";
+apiResponseStatus = "done";
+
+let hello;
+
+// type narrowing
+function msg(msg: string | number) {
+  if (typeof msg === "string") return "hello";
+  else return "enter valid message";
+}
+msg("hi");
+
+// type guard
+type response{
+    status:boolean,
+    data: string
+}
+function isResponseValid(response:any):response is response{
+    return(
+        typeof response.status === 'boolean' &&
+        typeof response.data === 'string'
+    )
+}
+function getResponse(response:response):string{
+if(isResponseValid(response)) return 'valid'
+return "not valid"
+}
